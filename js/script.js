d2c18a65c1aebe25c00315d89620d878187c6038
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-12 15:39:41 +0100

    added img

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 console.log('Vue ok', Vue);
+
+const app = Vue.createApp({
+    data(){
+        return{
+            currentIndex: 0,
+            data: {
+                user: {
+                  name: 'Nome Utente',
+                  avatar: '_io'
+                },
+                contacts: [
+                  {
+                    name: 'Michele',
+                    avatar: '_1',
+                  },
+                  {
+                    name: 'Fabio',
+                    avatar: '_2',
+                  },
+                  {
+                    name: 'Samuele',
+                    avatar: '_3',
+                  },
+                  {
+                    name: 'Luisa',
+                    avatar: '_4',
+                  },
+                ]
+              }
+        }
+    },
+});
+
+app.mount('#root');
