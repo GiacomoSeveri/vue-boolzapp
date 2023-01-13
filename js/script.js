74d508ed63fef30ef395e4ac92102f38e1ef335c
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-13 17:12:41 +0100

    problemi

--- js/script.js
+++ js/script.js
@@ -4,35 +4,107 @@ const app = Vue.createApp({
     data(){
         return{
             currentIndex: 0,
+            nowMessage: '',
             data: {
                 user: {
-                  name: 'Giacomo Severi',
+                  name: 'Fausto Gervasoni',
                   avatar: '_io'
                 },
                 contacts: [
                   {
-                    name: 'Michele',
+                    name: 'Brazorf',
                     avatar: '_1',
+                    visible: true,
+                    messages: [{
+                      date: '10/01/2020 15:30:55',
+                      text: 'Hai portato a spasso il cane?',
+                      status: 'sent'
+                    },
+                    {
+                      date: '10/01/2020 15:50:00',
+                      text: 'Ricordati di dargli da mangiare',
+                      status: 'sent'
+                    },
+                    {
+                      date: '10/01/2020 16:15:22',
+                      text: 'Tutto fatto!',
+                      status: 'received'
+                    }
+                    ],
                   },
                   {
-                    name: 'Fabio',
+                    name: 'Glamour',
                     avatar: '_2',
+                    visible: true,
+                    messages: [{
+                      date: '20/03/2020 16:30:00',
+                      text: 'Ciao come stai?',
+                      status: 'sent'
+                    },
+                    {
+                      date: '20/03/2020 16:30:55',
+                      text: 'Bene grazie! Stasera ci vediamo?',
+                      status: 'received'
+                    },
+                    {
+                      date: '20/03/2020 16:35:00',
+                      text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                      status: 'received'
+                    }
+                    ],
                   },
                   {
-                    name: 'Samuele',
+                    name: 'Nullazzo',
                     avatar: '_3',
+                    visible: true,
+                    messages: [{
+                      date: '28/03/2020 10:10:40',
+                      text: 'La Marianna va in campagna',
+                      status: 'received'
+                    },
+                    {
+                      date: '28/03/2020 10:20:10',
+                      text: 'Sicuro di non aver sbagliato chat?',
+                      status: 'sent'
+                    },
+                    {
+                      date: '28/03/2020 16:15:22',
+                      text: 'Ah scusa!',
+                      status: 'received'
+                    }
+                    ],
                   },
                   {
-                    name: 'Luisa',
+                    name: 'Caterina',
                     avatar: '_4',
+                    visible: true,
+                    messages: [{
+                      date: '10/01/2020 15:30:55',
+                      text: 'Lo sai che ha aperto una nuova pizzeria?',
+                      status: 'sent'
+                    },
+                    {
+                      date: '10/01/2020 15:50:00',
+                      text: 'Si, ma preferirei andare al cinema',
+                      status: 'received'
+                    }
+                    ],
                   },
                 ]
             }
         }
     },
     methods: {
-      changeChat(index){
+      changeChat(index) {
         this.currentIndex = index;
+      },
+      newMessage() {
+        if(this.nowMessage){
+          const now = {date: '10/01/2020 15:50:00', text: this.nowMessage, status: 'sent'};
+          this.contacts.message.push(now);
+          this.nowMessage = '';
+          this.$refs.focus();
+        }
       }
     }
 });
